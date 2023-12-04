88461a9ed1eb45c1b8212cadd126e29f85fe569a
Dipra <dipradas5940@gmail.com>
2023-12-05 00:17:59 +0600

    2.9

---
+++ src/nullable-unkonw-never.ts
@@ -0,0 +1,20 @@
+//  UNKNOWN TYPE
+const getMyCarSpeed = (speed: unknown) => {
+    if (typeof speed === 'number') {
+        const convertSpeed = (speed * 100) / 3600;
+        console.log(`My Speed ${convertSpeed}`);
+    }
+    if (typeof speed === 'string') {
+        const [value, unit] = speed.split(" ");
+        const convertedSpeed = (parseFloat(value) * 100) / 3600;
+        console.log(`${convertedSpeed}`);
+    }
+    else {
+        console.log('Wrong Type');
+    }
+}
+
+// NEVER 
+function throwError(message: string): never {
+    throw new Error(message);
+}
