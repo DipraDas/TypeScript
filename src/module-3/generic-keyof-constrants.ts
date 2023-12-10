36a3acbe5987268d9cc8254303740723fd726beb
Dipra <dipradas5940@gmail.com>
2023-12-10 22:28:54 +0600

    3.7

---
+++ src/module-3/generic-keyof-constrants.ts
@@ -0,0 +1,18 @@
+type personType = {
+    name: string,
+    age: number,
+    address: string
+}
+
+type newType = "name" | "age" | "address"; //manuallly korsi
+
+type newTypeUsingkeyof = keyof personType;
+
+const a: newType = "age";
+const b: newTypeUsingkeyof = "age";
+
+function getProperty<x, y extends keyof x>(obj: x, key: y) {
+    obj[key]
+}
+
+const property = getProperty({ name: 'Mr.X', age: 10 }, "age");
--- src/module-3/interface.ts
+++ src/module-3/interface.ts
@@ -1,39 +1,39 @@
-// type User = {
-//     name: string,
-//     age: number
-// }
+type User = {
+    name: string,
+    age: number
+}
 
-// type ExtendUser = User & {
-//     role: string
-// }
+type ExtendUser = User & {
+    role: string
+}
 
-// interface IUser {
-//     name: string,
-//     age: number
-// }
+interface IUser {
+    name: string,
+    age: number
+}
 
-// interface IExtendUser extends IUser {
-//     role: string
-// }
+interface IExtendUser extends IUser {
+    role: string
+}
 
-// const UserWithTypeAlias: User = {
-//     name: 'Dipra',
-//     age: 24
-// }
+const UserWithTypeAlias: User = {
+    name: 'Dipra',
+    age: 24
+}
 
-// const ooser: IExtendUser = {
-//     name: 'Ju',
-//     age: 46,
-//     role: 'bos'
-// }
+const ooser: IExtendUser = {
+    name: 'Ju',
+    age: 46,
+    role: 'bos'
+}
 
-// type addNumbersType = (num1: string, num2: string) => string;
-// const addNumbers: addNumbersType = (num1, num2) => num1 + num2;
+type addNumbersType = (num1: string, num2: string) => string;
+const addNumbers: addNumbersType = (num1, num2) => num1 + num2;
 
-// type rollNumberType = number[];
+type rollNumberType = number[];
 
-// interface IRollNumbers {
-//     [index: number]: number
-// }
+interface IRollNumbers {
+    [index: number]: number
+}
 
-// const rolllNumber: IRollNumbers = [1, 2, 3]
+const rolllNumber: IRollNumbers = [1, 2, 3]
